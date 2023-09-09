14bb7dd479eb4eb37a88c3bf1964e53f9f78d6fa
Lindsay Stewart <slindsay@amazon.com>
2023-09-09 03:27:07 -0700

    use_after_return test

---
+++ tests/unit/s2n_asan_use_after_return_test.c
@@ -0,0 +1,34 @@
+/*
+* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+#include "s2n_test.h"
+
+
+int *ptr;
+__attribute__((noinline))
+void FunctionThatEscapesLocalObject() {
+    int local[100] = { 0 };
+    ptr = &local[0];
+}
+
+int main()
+{
+    BEGIN_TEST();
+
+    FunctionThatEscapesLocalObject();
+    ptr[0] = 0;
+
+    END_TEST();
+}
