7c810855de6a7241ff0a05592b821ae789e28cf7
Lindsay Stewart <slindsay@amazon.com>
2023-10-28 23:28:32 -0700

    Put tests back

--- tests/unit/s2n_self_talk_tls12_test.c
+++ tests/unit/s2n_self_talk_tls12_test.c
@@ -29,52 +29,6 @@
 static const char *certificate_paths[SUPPORTED_CERTIFICATE_FORMATS] = { S2N_RSA_2048_PKCS1_CERT_CHAIN, S2N_RSA_2048_PKCS8_CERT_CHAIN };
 static const char *private_key_paths[SUPPORTED_CERTIFICATE_FORMATS] = { S2N_RSA_2048_PKCS1_KEY, S2N_RSA_2048_PKCS8_KEY };
 
-#define HISTOGRAM_SIZE 100
-static uint32_t histogram_values[HISTOGRAM_SIZE] = { 0 };
-static uint32_t histogram_counts[HISTOGRAM_SIZE] = { 0 };
-
-static int custom_mem_init(void)
-{
-    return 0;
-}
-
-static int custom_mem_cleanup(void)
-{
-    return 0;
-}
-
-static int custom_mem_malloc(void **ptr, uint32_t requested, uint32_t *allocated)
-{
-    int i;
-    for (i = 0; i < HISTOGRAM_SIZE; i++) {
-        if (histogram_values[i] == 0) {
-            histogram_values[i] = requested;
-        }
-
-        if (histogram_values[i] == requested) {
-            break;
-        }
-    }
-
-    if (i < HISTOGRAM_SIZE) {
-        histogram_counts[i] += 1;
-    }
-
-    *ptr = malloc(requested);
-    *allocated = requested;
-
-    /* Fill the memory with non-zeroes to check that s2n handles that fine */
-    memset(*ptr, 'a', requested);
-
-    return 0;
-}
-
-static int custom_mem_free(void *ptr, uint32_t size)
-{
-    free(ptr);
-    return 0;
-}
-
 void mock_client(struct s2n_test_io_pair *io_pair)
 {
     char buffer[0xffff];
@@ -89,9 +43,6 @@ void mock_client(struct s2n_test_io_pair *io_pair)
     config = s2n_config_new();
     s2n_config_disable_x509_verification(config);
     s2n_connection_set_config(conn, config);
-    conn->server_protocol_version = S2N_TLS12;
-    conn->client_protocol_version = S2N_TLS12;
-    conn->actual_protocol_version = S2N_TLS12;
 
     s2n_connection_set_io_pair(conn, io_pair);
 
@@ -125,7 +76,7 @@ void mock_client(struct s2n_test_io_pair *io_pair)
     /* Active application bytes consumed is reset to 0 in before writing data. */
     /* Its value should equal to bytes written after writing */
     ssize_t bytes_written = s2n_send(conn, buffer, i, &blocked);
-    if ((uint64_t) bytes_written != conn->active_application_bytes_consumed) {
+    if (bytes_written != conn->active_application_bytes_consumed) {
         exit(0);
     }
 
@@ -147,6 +98,8 @@ void mock_client(struct s2n_test_io_pair *io_pair)
 
 int main(int argc, char **argv)
 {
+    struct s2n_connection *conn;
+    struct s2n_config *config;
     s2n_blocked_status blocked;
     int status;
     pid_t pid;
@@ -154,20 +107,9 @@ int main(int argc, char **argv)
     char *private_key_pem;
     char *dhparams_pem;
 
-    /* We have to set the callback before BEGIN_TEST, because s2n_init() is called
-     * there.
-     */
-    int rc = s2n_mem_set_callbacks(custom_mem_init, custom_mem_cleanup, custom_mem_malloc, custom_mem_free);
-
     BEGIN_TEST();
     EXPECT_SUCCESS(s2n_disable_tls13_in_test());
-
-    /* Can't add callbacks if s2n is initialized */
-    EXPECT_FAILURE(s2n_mem_set_callbacks(custom_mem_init, custom_mem_cleanup, custom_mem_malloc, custom_mem_free));
-
-    EXPECT_SUCCESS(rc);
-
-    for (size_t is_dh_key_exchange = 0; is_dh_key_exchange <= 1; is_dh_key_exchange++) {
+    for (int is_dh_key_exchange = 0; is_dh_key_exchange <= 1; is_dh_key_exchange++) {
         struct s2n_cert_chain_and_key *chain_and_keys[SUPPORTED_CERTIFICATE_FORMATS];
 
         /* Create a pipe */
@@ -188,19 +130,13 @@ int main(int argc, char **argv)
         EXPECT_NOT_NULL(private_key_pem = malloc(S2N_MAX_TEST_PEM_SIZE));
         EXPECT_NOT_NULL(dhparams_pem = malloc(S2N_MAX_TEST_PEM_SIZE));
 
-        DEFER_CLEANUP(struct s2n_config *config = s2n_config_new(),
-                s2n_config_ptr_free);
-        DEFER_CLEANUP(struct s2n_connection *conn = s2n_connection_new(S2N_SERVER),
-                s2n_connection_ptr_free);
-
         /* This is the server process, close the client end of the pipe */
         EXPECT_SUCCESS(s2n_io_pair_close_one_end(&io_pair, S2N_CLIENT));
 
-        conn->server_protocol_version = S2N_TLS12;
-        conn->client_protocol_version = S2N_TLS12;
-        conn->actual_protocol_version = S2N_TLS12;
+        EXPECT_NOT_NULL(conn = s2n_connection_new(S2N_SERVER));
 
-        for (size_t cert = 0; cert < SUPPORTED_CERTIFICATE_FORMATS; cert++) {
+        EXPECT_NOT_NULL(config = s2n_config_new());
+        for (int cert = 0; cert < SUPPORTED_CERTIFICATE_FORMATS; cert++) {
             EXPECT_SUCCESS(s2n_read_test_pem(certificate_paths[cert], cert_chain_pem, S2N_MAX_TEST_PEM_SIZE));
             EXPECT_SUCCESS(s2n_read_test_pem(private_key_paths[cert], private_key_pem, S2N_MAX_TEST_PEM_SIZE));
             EXPECT_NOT_NULL(chain_and_keys[cert] = s2n_cert_chain_and_key_new());
@@ -220,6 +156,7 @@ int main(int argc, char **argv)
 
         /* Negotiate the handshake. */
         EXPECT_SUCCESS(s2n_negotiate(conn, &blocked));
+        EXPECT_EQUAL(conn->actual_protocol_version, S2N_TLS12);
 
         char buffer[0xffff];
         for (int i = 1; i < 0xffff; i += 100) {
@@ -248,57 +185,22 @@ int main(int argc, char **argv)
             EXPECT_TRUE(shutdown_rc == 0 || (errno == EAGAIN && blocked));
         } while (shutdown_rc != 0);
 
-        for (size_t cert = 0; cert < SUPPORTED_CERTIFICATE_FORMATS; cert++) {
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+        for (int cert = 0; cert < SUPPORTED_CERTIFICATE_FORMATS; cert++) {
             EXPECT_SUCCESS(s2n_cert_chain_and_key_free(chain_and_keys[cert]));
         }
+        EXPECT_SUCCESS(s2n_config_free(config));
 
         /* Clean up */
         EXPECT_EQUAL(waitpid(-1, &status, 0), pid);
         EXPECT_EQUAL(status, 0);
         EXPECT_SUCCESS(s2n_io_pair_close_one_end(&io_pair, S2N_SERVER));
+
         free(cert_chain_pem);
         free(private_key_pem);
         free(dhparams_pem);
     }
 
-#if defined(S2N_TEST_DEBUG)
-    /* Sort our histogram */
-    uint32_t spare_value, spare_count;
-    for (int i = 0; i < HISTOGRAM_SIZE; i++) {
-        if (histogram_counts[i] == 0) {
-            break;
-        }
-
-        for (int j = i + 1; j < HISTOGRAM_SIZE; j++) {
-            if (histogram_counts[j] == 0) {
-                break;
-            }
-
-            if (histogram_values[j] < histogram_values[i]) {
-                spare_value = histogram_values[i];
-                spare_count = histogram_counts[i];
-
-                histogram_values[i] = histogram_values[j];
-                histogram_counts[i] = histogram_counts[j];
-
-                histogram_values[j] = spare_value;
-                histogram_counts[j] = spare_count;
-            }
-        }
-    }
-
-    /* Print the histogram values */
-    TEST_DEBUG_PRINT("\n\n");
-    for (int i = 0; i < HISTOGRAM_SIZE; i++) {
-        if (histogram_values[i] == 0) {
-            break;
-        }
-        TEST_DEBUG_PRINT("Allocated %d bytes %d times\n", histogram_values[i], histogram_counts[i]);
-    }
-    TEST_DEBUG_PRINT("\n");
-#endif
-
     END_TEST();
-
     return 0;
 }
